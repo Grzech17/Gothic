1cfef7e3327c9afbbae907a3bd72d3b18ec78312
Grzech17 <165683709+Grzech17@users.noreply.github.com>
2024-05-02 20:36:20 +0200

    Update GOTHIC_GAME_Mech.cpp

--- GOTHIC_GAME_Mech.cpp
+++ GOTHIC_GAME_Mech.cpp
@@ -7,8 +7,8 @@ extern int koniec, czystka, quest, pogadane, zadlo;
 int expienie(postac *dobry, postac *zly);
 int mech(postac *dobry, postac *zly, bool musisz = 0)											
 {
-	int hp = dobry->hp;																			//do wyleczenia postaci
-	int reset = zly->hp; 																		//do wyleczenia przeciwnika
+	int hp = dobry->hp;											//do wyleczenia postaci
+	int reset = zly->hp; 											//do wyleczenia przeciwnika
 	cout<<"Spotykasz "<<zly->nazwa<<"a"<<endl													
 	    <<dobry->nazwa<<": HP-"<<dobry->hp<<" DMG-"<<dobry->dmg<<" DEF-"<<dobry->def<<""<<endl
 	    <<zly->nazwa<<": HP-"<<zly->hp<<" DMG-"<<zly->dmg<<" DEF-"<<zly->def<<""<<endl;
@@ -28,43 +28,43 @@ int mech(postac *dobry, postac *zly, bool musisz = 0)
 	else atakujesz = 1;
 	if(atakujesz == 1)
 	{
-		cout<<"Wyci¹gasz broñ i szar¿ujesz..."<<endl;
+		cout<<"WyciÄ…gasz broÅ„ i szarÅ¼ujesz..."<<endl;
 		cin.ignore(); cin.ignore();
 		while(dobry->hp>0 && zly->hp>0)
 		{
-			int traf = rand() % 3;																//67% szans na twoje trafienie
+			int traf = rand() % 3;									//67% szans na twoje trafienie
 			if(traf >= 1)
 			{
-				if(dobry->dmg<=zly->def)														//jeœli twój dmg jest mniejszy ni¿ def przeciwnika
+				if(dobry->dmg<=zly->def)							//jeÅ›li twÃ³j dmg jest mniejszy niÅ¼ def przeciwnika
 				{
-					zly->hp--;																	// -1hp
-					cout<<"TRAFIENIE! Smyrn¹³eœ przeciwnika za 1 pkt obra¿eñ"<<endl
+					zly->hp--;								// -1hp
+					cout<<"TRAFIENIE! SmyrnÄ…Å‚eÅ› przeciwnika za 1 pkt obraÅ¼eÅ„"<<endl
 						<<"HP przeciwnika: "<<zly->hp<<""<<endl;
 				}
 				else
 				{
-				zly->hp = zly->hp - dobry->dmg + zly->def;										//dmg - def
-				cout<<"TRAFIENIE! Zadajesz "<<(dobry->dmg-zly->def)<<" obra¿eñ"<<endl
+				zly->hp = zly->hp - dobry->dmg + zly->def;					//dmg - def
+				cout<<"TRAFIENIE! Zadajesz "<<(dobry->dmg-zly->def)<<" obraÅ¼eÅ„"<<endl
 				    <<"HP przeciwnika: "<<zly->hp<<""<<endl;
 				}
-				if(zly->hp<1)																	//zabijasz
+				if(zly->hp<1)									//zabijasz
 				{
 					cout<<"Jedna bestia mniej!"<<endl
 						<<"-----------------------"<<endl;
-					if(pogadane == 1 && zly->nazwa == "Wilk" && quest<2) 						//quest diego na wilki
+					if(pogadane == 1 && zly->nazwa == "Wilk" && quest<2) 			//quest diego na wilki
 					{
 						quest++;
-						cout<<"Zabito "<<quest<<" z 2 Wilków"<<endl;
+						cout<<"Zabito "<<quest<<" z 2 WilkÃ³w"<<endl;
 					}
-					if(zly->nazwa == "Krwiopijca" && zadlo<3)									//quest dextera na krwiopijce
+					if(zly->nazwa == "Krwiopijca" && zadlo<3)				//quest dextera na krwiopijce
 					{
 						zadlo++;
-						cout<<"Zebrano ¿¹d³o"<<endl
+						cout<<"Zebrano Å¼Ä…dÅ‚o"<<endl
 							<<"Masz "<<zadlo<<" w ekwipunku"<<endl;
 					}
-					dobry->hp = hp;																//wyleczenie postaci
-					if(zly->lvl == 99) koniec = 1;												//œmieræ gomeza
-					expienie(dobry, zly);														//zdobycie expa
+					dobry->hp = hp;								//wyleczenie postaci
+					if(zly->lvl == 99) koniec = 1;						//Å›mierÄ‡ gomeza
+					expienie(dobry, zly);							//zdobycie expa
 					continue;
 				}
 			}
@@ -73,24 +73,24 @@ int mech(postac *dobry, postac *zly, bool musisz = 0)
 				cout<<"CHYBIENIE"<<endl
 				    <<"HP przeciwnika: "<<zly->hp<<""<<endl;
 			}
-			traf = rand() % 2;																	//50% szansa na twój unik
+			traf = rand() % 2;									//50% szansa na twÃ³j unik
 			if(traf)
 			{
 				if(zly->dmg<=dobry->def)
 				{
 					dobry->hp--;
-					cout<<"OUCH! Przeciwnik smyrn¹³ Ciê za 1 pkt obra¿eñ"<<endl
+					cout<<"OUCH! Przeciwnik smyrnÄ…Å‚ CiÄ™ za 1 pkt obraÅ¼eÅ„"<<endl
 						<<"Twoje: "<<dobry->hp<<""<<endl;
 				}
 				else
 				{
 				dobry->hp = dobry->hp - zly->dmg + dobry->def;
-				cout<<"OUCH! Otrzymujesz "<<(zly->dmg-dobry->def)<<" obra¿eñ"<<endl
+				cout<<"OUCH! Otrzymujesz "<<(zly->dmg-dobry->def)<<" obraÅ¼eÅ„"<<endl
 					<<"Twoje HP: "<<dobry->hp<<""<<endl;
 				}
-				if(dobry->hp<1)																			//twoja œmieræ
+				if(dobry->hp<1)									//twoja Å›mierÄ‡
 				{
-					cout<<"zdech³eœ xd"<<endl;
+					cout<<"zdechÅ‚eÅ› xd"<<endl;
 					cin.ignore();
 					cin.get();
 					exit(0);
@@ -104,11 +104,11 @@ int mech(postac *dobry, postac *zly, bool musisz = 0)
 			}
 		cin.ignore();
 		}
-	zly->hp = reset;																					//wyleczenie przeciwnika
+	zly->hp = reset;											//wyleczenie przeciwnika
 	}
 	else
 		{
-			cout<<"Stój, œmieciu!"<<endl
+			cout<<"StÃ³j, Å›mieciu!"<<endl
 				<<"----------------"<<endl;
 			cin.ignore();
 		}
